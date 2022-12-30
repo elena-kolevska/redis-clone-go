9a11c1238480d380341b467b2dea0b6ae77c05a3
Elena Kolevska <elena@kolevska.com>
2022-12-30 16:43:09 +0000

    GET and SET working

--- app/server.go
+++ app/server.go
@@ -4,11 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"strconv"
-
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Type byte
@@ -22,15 +21,6 @@ const (
 type Value struct {
 	typ   Type
 	bytes []byte
-	array []Value
-}
-
-func (v Value) Array() []Value {
-	if v.typ == Array {
-		return v.array
-	}
-
-	return []Value{}
 }
 
 func (v Value) String() string {
@@ -42,7 +32,6 @@ func (v Value) String() string {
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -51,17 +40,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	//Initialize the data store
+	storage := NewStorage()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, storage)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, storage *Storage) {
 	defer conn.Close()
 	for {
 		value, err := DecodeRESP(bufio.NewReader(conn))
@@ -70,14 +62,20 @@ func handleConnection(conn net.Conn) {
 			return // Ignore clients that we fail to read from
 		}
 
-		command := value.Array()[0].String()
-		args := value.Array()[1:]
+		command := strings.Fields(value.String())[0]
+		command = strings.ToLower(command)
+		args := strings.Fields(value.String())[1:]
 
 		switch command {
 		case "ping":
 			conn.Write([]byte("+PONG\r\n"))
 		case "echo":
-			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String()), args[0].String())))
+			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(string(args[0])), string(args[0]))))
+		case "set":
+			storage.Set(args[0], args[1])
+			conn.Write([]byte("+OK\r\n"))
+		case "get":
+			conn.Write([]byte(fmt.Sprintf("+%s\r\n", storage.Get(args[0]))))
 		default:
 			conn.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
 		}
@@ -96,8 +94,6 @@ func DecodeRESP(byteStream *bufio.Reader) (Value, error) {
 		return decodeSimpleString(byteStream)
 	case "$":
 		return decodeBulkString(byteStream)
-	case "*":
-		return decodeArray(byteStream)
 	}
 
 	return Value{}, fmt.Errorf("invalid RESP data type byte: %s", string(dataTypeByte))
@@ -135,34 +131,6 @@ func decodeBulkString(byteStream *bufio.Reader) (Value, error) {
 	return Value{
 		typ:   BulkString,
 		bytes: readBytes[:count],
-	}, nil
-}
-
-func decodeArray(byteStream *bufio.Reader) (Value, error) {
-	readBytesForCount, err := readUntilCRLF(byteStream)
-	if err != nil {
-		return Value{}, fmt.Errorf("failed to read bulk string length: %s", err)
-	}
-
-	count, err := strconv.Atoi(string(readBytesForCount))
-	if err != nil {
-		return Value{}, fmt.Errorf("failed to parse bulk string length: %s", err)
-	}
-
-	array := []Value{}
-
-	for i := 1; i <= count; i++ {
-		value, err := DecodeRESP(byteStream)
-		if err != nil {
-			return Value{}, err
-		}
-
-		array = append(array, value)
-	}
-
-	return Value{
-		typ:   Array,
-		array: array,
 	}, nil
 }
 
